cbe2400e52922212f70202e0cb9a2095baa1dbbe
Logic-seeker <shobhabstha@gmail.com>
2012-01-15 12:42:07 +0545

    initial payload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RevolTechGithubCom::Application.config.secret_token = '7a04a6c000a967395c89907fa264783d8f24025604265cf3c38f4fa2cf876fe8065829522eb928445fcf4e32409eb1f9631896e13063813f6904b6edbfb4f0c4'
