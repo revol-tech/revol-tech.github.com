61e22e4706dec503808e2152e4881ecee8b6a860
kapil Nakhwa <xecutioner.kapil@gmail.com>
2012-02-01 22:46:24 +0545

    cjanged the rvm ruby version and rvm type flag in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,8 +25,8 @@ $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 # Load RVM's capistrano plugin.
 require "rvm/capistrano"
 
-set :rvm_ruby_string, '1.9.2'
-set :rvm_type, :user  # Don't use system-wide RVM
+set :rvm_ruby_string, '1.9.3'
+#set :rvm_type, :user  # Don't use system-wide RVM
 
 
 # if you're still using the script/reaper helper you will need
