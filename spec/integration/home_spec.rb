066c860b285b32f323782a160b2c9b59116aa6a2
kapil Nakhwa <xecutioner.kapil@gmail.com>
2012-02-11 02:17:38 +0545

    started home page tests

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -1,9 +1,32 @@
 require 'spec_helper'
 
 describe 'home page' do
+  before(:each) do
+  visit '/'
+  end
 
   it 'should show our tagline' do
-    visit '/'
     page.should have_content('Revolutionizing lives through technology')
   end
+
+  it 'should show current day' do
+    page.should have_content("#{Time.new.strftime("Happy %A")}")
+  end
+
+  it 'should have top navigation bar' do
+    page.should have_selector 'div', :class => 'nav'
+  end
+
+  it 'should have our logo' do
+    page.should have_selector 'div', :class => 'rotator'
+  end
+
+  it 'should show our business motto' do
+    page.should have_content('Smarter business through smarter web apps.')
+  end
+
+   it 'should have footer' do
+    page.should have_selector 'footer'
+  end
+
 end
