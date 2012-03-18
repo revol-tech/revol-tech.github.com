06d0de7b8f7be2ab448b04265fb5e027686078af
kapil Nakhwa <xecutioner.kapil@gmail.com>
2012-03-18 23:14:04 +0545

    fixed model name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120318165920) do
+ActiveRecord::Schema.define(:version => 20120318170439) do
 
   create_table "projects", :force => true do |t|
     t.string   "title"
