9789cea7a69b7c6aa2618fda61956606cff95f6c
kuber <kuberaaganja@gmail.com>
2012-03-21 01:40:05 +0545

    user project association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120320172824) do
+ActiveRecord::Schema.define(:version => 20120320192503) do
 
   create_table "projects", :force => true do |t|
     t.string   "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(:version => 20120320172824) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin"
+    t.integer  "project_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
