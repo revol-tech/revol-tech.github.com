f1f40bd5333086ab00a37944513f52ffe6614918
kapil Nakhwa <xecutioner.kapil@gmail.com>
2012-03-20 23:37:35 +0545

    added repo to project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120320153228) do
+ActiveRecord::Schema.define(:version => 20120320172824) do
 
   create_table "projects", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20120320153228) do
     t.datetime "end_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "repo"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
