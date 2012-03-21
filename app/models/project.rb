f5252cd07d5bee3958786ef4cf359ce97be614af
kapil Nakhwa <xecutioner.kapil@gmail.com>
2012-03-22 01:45:15 +0545

    added the developers list in the projects show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,16 @@
 class Project < ActiveRecord::Base
   belongs_to :user
+  DEVELOPERS = {
+  "xecutioner" => "Kapil Raj Nakhwa",
+  "nfloyd" => "Niraj Pradhan",
+  "kakshpati" => "Anup kakshpati",
+  "logic-seeker" => "Shobhab Shrestha",
+  "shiv1j" => "Shivan Jung Thapa",
+  "chucha" => "Niraj Shakya",
+  "dhami" => "Ramesh Dhami",
+  "julfikar" => "Julfiquar Ali Ahmed",
+  "aaganja" => "Kuber Aaganja",
+  }
 
   def initialize_github
     github = Github.new do |opts|
@@ -8,6 +19,18 @@ class Project < ActiveRecord::Base
       opts.login = "revol-tech"
       opts.password = "bhaktapur11"
     end
+  end
+
+  def get_repo
+    github_handle = self.initialize_github
+    github_handle.repos.get_repo("revol-tech",self.repo)
+  end
+
+  def get_developers
+    developers = []
+    self.initialize_github.repos.collaborators(self.initialize_github.user,self.repo).each { |e| developers << e['login'] }
+    developers.keep_if{|e| DEVELOPERS.keys.include?(e)}
+    developers.collect!{|e| DEVELOPERS[e].humanize}
   end
 
 end
