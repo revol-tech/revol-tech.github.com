18db078c69d12dd3f76937bd97fbecb7018977b1
kuber <kuberaaganja@gmail.com>
2012-03-22 01:47:20 +0545

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-["user@gmail.com|password|false","admin@gmail.com|bhaktapur11|true"].each do |t|
-    email,encrypted_password,admin = t.chomp.split("|")
-    User.create!(:email => email, :password => encrypted_password,:admin=>admin)
+["1|users3@gmail.com|password|false|user|malainabirseu|Banesor|4444444","2|admins3@gmail.com|bhaktapur11|true|admin|revoltech|Shallaghari|6666666"].each do |t|
+    id,email,encrypted_password,admin,name,organization,address,phone_no = t.chomp.split("|")
+    User.create!(:id => id, :email => email, :password => encrypted_password,:admin=>admin,:name=>name,:organization=>organization,:address=>address,:phone_no=>phone_no)
+  end
+
+["client|1"].each do |t|
+    client,user_id = t.chomp.split("|")
+    Project.create!(:client => client, :user_id =>user_id)
   end
