d283252c7690b5252755848a9c489f92fd225897
revol-tech <revolt.techno@gmail.com>
2012-03-21 13:07:44 +0545

    #33 footer fixed using css

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
+
 ActiveRecord::Schema.define(:version => 20120320192503) do
 
   create_table "projects", :force => true do |t|
