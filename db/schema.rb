258ab262d8666f2bbc1f71ad1c550333b26f25cb
Logic-Seeker <shobhabstha@gmail.com>
2012-05-13 12:42:01 +0545

    shift from sqlite to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,59 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120406093255) do
+ActiveRecord::Schema.define(:version => 20120420070611) do
+
+  create_table "active_admin_comments", :force => true do |t|
+    t.integer  "resource_id",   :null => false
+    t.string   "resource_type", :null => false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "namespace"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
+
+  create_table "activities", :force => true do |t|
+    t.string   "headline"
+    t.string   "image_path"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "text"
+  end
+
+  create_table "admin_users", :force => true do |t|
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
+  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "attendances", :force => true do |t|
     t.datetime "date_time"
     t.integer  "employee_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "banners", :force => true do |t|
+    t.string   "image_path"
+    t.string   "caption"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -28,12 +76,30 @@ ActiveRecord::Schema.define(:version => 20120406093255) do
     t.string   "attendance_password"
   end
 
+  create_table "jobs", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "address"
+    t.integer  "contact_no"
+    t.string   "cv"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "messages", :force => true do |t|
     t.string   "from"
     t.string   "to"
     t.text     "subject"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "news_and_events", :force => true do |t|
+    t.string   "headline"
+    t.string   "image_path"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "text"
   end
 
   create_table "projects", :force => true do |t|
@@ -55,7 +121,7 @@ ActiveRecord::Schema.define(:version => 20120406093255) do
     t.integer  "item"
     t.string   "table"
     t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 5
+    t.integer  "year",       :limit => 8
     t.datetime "created_at"
     t.datetime "updated_at"
   end
